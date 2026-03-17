571b5376cf9bdb669a0b091a8803ae181ccf732d
JoelA-07 <2312053@nec.edu.in>
2026-03-17 16:24:12 +0530

    fixed student fetching by associating the studentcourse in studentdetails

--- backend/models/acadamic/studentDetails.js
+++ backend/models/acadamic/studentDetails.js
@@ -1,6 +1,27 @@
 import StudentDetails from '../student/StudentDetails.js';
 
 export default (sequelize) => {
+  // Extend associations for academic models that include StudentDetails
+  // (e.g., StudentCourse, StudentElectiveSelection).
+  const existingAssociate = StudentDetails.associate;
+  StudentDetails.associate = (models) => {
+    if (typeof existingAssociate === 'function') existingAssociate(models);
+
+    if (models.StudentCourse) {
+      StudentDetails.hasMany(models.StudentCourse, {
+        foreignKey: 'regno',
+        sourceKey: 'registerNumber'
+      });
+    }
+
+    if (models.StudentElectiveSelection) {
+      StudentDetails.hasMany(models.StudentElectiveSelection, {
+        foreignKey: 'regno',
+        sourceKey: 'registerNumber'
+      });
+    }
+  };
+
   // Returns the unified model from the main student folder
   return StudentDetails;
 };
